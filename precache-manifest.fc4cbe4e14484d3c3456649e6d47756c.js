e5f62295145a7578eb7af44d860bd6af1f2bf522
priyanshu singh <priyanshuchotu1906@gmail.com>
2021-09-21 11:09:16 +0530

    Updates

---
+++ precache-manifest.fc4cbe4e14484d3c3456649e6d47756c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1073fc867e4dd72bc9191b241d02fbf2",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "6fed90736ff62aa55ddc",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "610bdc9597ae3714515e",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fed90736ff62aa55ddc",
+    "url": "/box-office/static/js/main.e70f7bea.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
